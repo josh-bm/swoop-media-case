36027460f5b81e51d27656d8e032991e1f2202fe
josh-bm <32262804+josh-bm@users.noreply.github.com>
2022-11-11 13:17:32 +0100

    Added fetch functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import localFont from "@next/font/local";
+const myFont = localFont({ src: "../fonts/Pixelar-Regular-W01-Regular.ttf" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={myFont.className}>
+      <Component {...pageProps} />;
+    </div>
+  );
 }
