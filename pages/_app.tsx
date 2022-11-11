45ca9d1dd3d6765b9f96f6d1665516ec196b8e6b
josh-bm <32262804+josh-bm@users.noreply.github.com>
2022-11-11 13:41:32 +0100

    Added Layout to homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,28 @@
 import "../styles/normalize.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
+import { ReactElement, ReactNode } from "react";
+import { NextPage } from "next";
 
 import localFont from "@next/font/local";
 const myFont = localFont({ src: "../fonts/Pixelar-Regular-W01-Regular.ttf" });
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  return getLayout(
     <div className={myFont.className}>
-      <Component {...pageProps} />;
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
     </div>
   );
 }
